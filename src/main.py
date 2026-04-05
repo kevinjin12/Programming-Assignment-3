692030d270f447ea2d5c28f1aed42b494da6ff33
Keerat Kohli <kkeerat012@gmail.com>
2026-04-05 15:31:24 -0400

    Wrote the backtracking for actually getting the optimal subsequence

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ def hvlcs(A, B, values):
     n = len(B)
  
     dp = [[0] * (n + 1) for i in range(m + 1)]
- 
+    
+    # Calculating the Value
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             # If character is the same, add the value of character
@@ -12,9 +13,22 @@ def hvlcs(A, B, values):
             # If character is different, take the string with max score
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
- 
-    return dp[m][n]
+    
+    # Getting the Optimal Subsequence (Backtracking)
+    i, j = m, n
+    subsequence = ""
+    while i >= 0 and j >= 0:
+        if dp[i][j] != dp[i - 1][j] and dp[i][j] != dp[i][j - 1]:
+            subsequence += A[i - 1]
+            i, j = i - 1, j - 1
+        elif dp[i][j] == dp[i - 1][j]:
+            i -= 1
+        elif dp[i][j] == dp[i][j - 1]:
+            j -= 1
 
+    subsequence = subsequence[::-1]
+
+    return dp[m][n], subsequence
 
 def main():
     A = "aacb"
@@ -25,8 +39,9 @@ def main():
         'c': 5,
     }
 
-    res = hvlcs(A, B, values)
+    res, subsequence = hvlcs(A, B, values)
     print(res)
+    print(subsequence)
 
 if __name__ == "__main__":
     main()
